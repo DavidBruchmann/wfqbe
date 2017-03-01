1f77b21ae7b38b107c12dfe25ca488714f0b2757
David Bruchmann <david.bruchmann@gmail.com>
2017-03-02 04:23:47 +0700

    missing class-folder

---
+++ Classes/class.tx_wfqbe_mod1_ajax.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Created on 09/gen/09
+ * Author mauro
+ * 
+ */
+ 
+ class tx_wfqbe_mod1_ajax	{
+ 	
+ 	function tx_wfqbe_mod1_ajax()	{}
+ 	
+ 	public function ajaxFieldTypeHelp($params, &$ajaxObj) {
+	    //$this->init();
+	    $LANG = $GLOBALS['LANG'];
+	    $LANG->includeLLFile('EXT:wfqbe/mod1/locallang.xml');
+	    
+        // the content is an array that can be set through $key / $value pairs as parameter
+        $ajaxObj->addContent('help', $LANG->getLL('help_'.\TYPO3\CMS\Core\Utility\GeneralUtility::_GP('field')).'<br />');
+	}
+ 	
+ }
